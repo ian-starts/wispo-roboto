24c68104c5b0ec0816e65d558ea649e6789e3f1c
Yoni Ian Kok <hello@yik.dev>
2021-11-30 18:15:26 +0100

    remove some stuff

--- app/main.py
+++ app/main.py
@@ -5,10 +5,6 @@ import telegram
 app = Flask(__name__)
 
 TELEGRAM_API_KEY = "2120742951:AAF308uoeBHAhOASiVlBBNK8VQskGfeVLbY"
-TELEGRAM_CHAT_ID = -509915845
-WEATHER_API_KEY = "b70d028fde6bce370cd3e58d5e428e86"
-WEATHER_API_ID = "e4f7f0bf"
-RESORT_ID = 333010
  
 @app.route("/message", methods=['post'])
 def message_stuff():
