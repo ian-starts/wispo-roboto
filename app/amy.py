125f3125bd12046f011e2c7b2859ab68636c7848
Amy <a.oey@students.uu.nl>
2021-12-03 15:23:20 +0100

    Off with that apenstaartje

--- app/amy.py
+++ app/amy.py
@@ -6,18 +6,16 @@ names = ["Joost", "Yoni", "Rutger", "AmyO", "AmyL", "Thijs", "Joshiwa","Lenne"]
 
 def get_rng(input):
     if input == "rng":
-        output = "please use this function as follows: rng@{number}, for the random number generator"
-    elif "rng@" in input:
+        output = "please use this function as follows: rng{number}, for the random number generator"
+    elif "rng" in input:
         try:
-            new = re.findall(r'rng@(\w+)', input)[0]
-            print(new)
+            new = re.findall(r'rng(\w+)', input)[0]
             components = re.split('(\d+)',new)
-            print([int(word) for word in components if word.isdigit()][0])
-            output = "The magic number is : " + random.randint(1,[int(word) for word in components if word.isdigit()][0])
+            output = "The magic number is : " + str(random.randint(1,[int(word) for word in components if word.isdigit()][0]))
         except:
-            output = "You are probably doing something wrong, you fool. Format: rng@{number}"
+            output = "You are probably doing something wrong, you fool. Format: rng{number}"
     else:
-        output = "yeeez, just use it the way you are supposed to, idiot. Format: rng@{number}"
+        output = "yeeez, just use it the way you are supposed to, idiot. Format: rng{number}"
     return output
 
 def get_name():
