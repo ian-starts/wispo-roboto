0256b1fc263a40243a99e9be0e0d07e15ffc1529
Yoni Ian Kok <hello@yik.dev>
2021-12-04 11:53:45 +0100

    editor reformat

--- app/amy.py
+++ app/amy.py
@@ -3,8 +3,9 @@ import random
 import requests
 import geopy.distance
 
-#Je kan de namen niet opvragen in de Telegram API :(((
-names = ["Joost", "Yoni", "Rutger", "AmyO", "AmyL", "Thijs", "Joshiwa","Lenne"]
+# Je kan de namen niet opvragen in de Telegram API :(((
+names = ["Joost", "Yoni", "Rutger", "AmyO", "AmyL", "Thijs", "Joshiwa", "Lenne"]
+
 
 def get_rng(input):
     if input == "rng":
@@ -12,43 +13,48 @@ def get_rng(input):
     elif "rng" in input:
         try:
             new = re.findall(r'rng(\w+)', input)[0]
-            components = re.split('(\d+)',new)
-            output = "The magic number is : " + str(random.randint(1,[int(word) for word in components if word.isdigit()][0]))
+            components = re.split('(\d+)', new)
+            output = "The magic number is : " + str(
+                random.randint(1, [int(word) for word in components if word.isdigit()][0]))
         except:
             output = "You are probably doing something wrong, you fool. Format: rng{number}"
     else:
         output = "yeeez, just use it the way you are supposed to, idiot. Format: rng{number}"
     return output
 
+
 def get_name():
     return random.choice(names)
 
+
 def get_manly():
-    nmb = random.randint(1,30)
-    return ("8"+nmb*'='+"D")
+    nmb = random.randint(1, 30)
+    return ("8" + nmb * '=' + "D")
+
 
 def get_location(input):
     try:
         location = re.findall(r'dist(\w+)', input)[0]
     except:
         output = "You did not enter your text correctly. Format /dist{location} e.g. Rotterdam"
-    
+
     try:
         URL = "https://geocode.search.hereapi.com/v1/geocode"
-        api_key = '91Rke_Qm2yDiF-zEvHEJfN9-U0FKeK4bjF4I2S1Je7A' # Acquire from developer.here.com
-        PARAMS = {'apikey':api_key,'q':location} 
+        api_key = '91Rke_Qm2yDiF-zEvHEJfN9-U0FKeK4bjF4I2S1Je7A'  # Acquire from developer.here.com
+        PARAMS = {'apikey': api_key, 'q': location}
 
-        r = requests.get(url = URL, params = PARAMS) 
+        r = requests.get(url=URL, params=PARAMS)
         data = r.json()
 
         latitude = data['items'][0]['position']['lat']
         longitude = data['items'][0]['position']['lng']
-        coords_1 = (latitude,longitude)
-        coords_2 = (45.01331, 6.12471) # coord les deux alpes
-        
-        output = "You are " + str(round(geopy.distance.distance(coords_1, coords_2).km)) + " kilometers away from your WISPO destination: Les Deux Alpes ⛷️🏂"
-    
+        coords_1 = (latitude, longitude)
+        coords_2 = (45.01331, 6.12471)  # coord les deux alpes
+
+        output = "You are " + str(round(geopy.distance.distance(coords_1,
+                                                                coords_2).km)) + " kilometers away from your WISPO destination: Les Deux Alpes ⛷️🏂"
+
     except:
         output = "You did not enter a existing location. Format /dist{location} e.g. Rotterdam"
-    
+
     return output
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, Response
+from flask import Flask, request, Response
 from datetime import date, timedelta
 import requests
 import telegram
@@ -16,42 +16,45 @@ def message_stuff():
     request_data = request.get_json()
     print(request_data)
     if 'lol' in request_data['message']['text']:
-        send_message(bot, "lol to you, nerd!",request_data['message']['chat']['id'])
+        send_message(bot, "lol to you, nerd!", request_data['message']['chat']['id'])
     elif "joke" in request_data["message"]["text"]:
         joke = get_joke()
         send_message(bot, joke, request_data["message"]["chat"]["id"])
     elif 'mountainview' in request_data['message']['text']:
         mountain_image_data = get_mountain_image()
-        send_image(bot, mountain_image_data["medias"][0]["urls"]["large"], request_data['message']['chat']['id'], mountain_image_data["medias"][0]["date"])
+        send_image(bot, mountain_image_data["medias"][0]["urls"]["large"], request_data['message']['chat']['id'],
+                   mountain_image_data["medias"][0]["date"])
     elif "rng" in request_data['message']['text']:
         number = get_rng(request_data['message']['text'])
         send_message(bot, number, request_data["message"]["chat"]["id"])
     elif "dishes" in request_data['message']['text']:
         name = get_name()
-        text = "Today, " + name + " will be doing the dishes!! LOL loser 😙" 
+        text = "Today, " + name + " will be doing the dishes!! LOL loser 😙"
         send_message(bot, text, request_data["message"]["chat"]["id"])
     elif "size" in request_data['message']['text']:
         size = get_manly()
-        send_message(bot, size,request_data['message']['chat']['id'])
+        send_message(bot, size, request_data['message']['chat']['id'])
     elif "dist" in request_data['message']['text']:
         distance = get_location(request_data['message']['text'])
-        send_message(bot, distance,request_data['message']['chat']['id'])
+        send_message(bot, distance, request_data['message']['chat']['id'])
     return Response("", status=202, mimetype='application/json')
 
 
 def get_mountain_image():
     request_data = {
-            'types': 'image',
-            'api_key': 'wJYTf-gyLNX-tk7ll-cGviT',
-            'center': (date.today() + timedelta(days=1)).strftime('%Y-%m-%d'),
-            'count' : 1
-        }
+        'types': 'image',
+        'api_key': 'wJYTf-gyLNX-tk7ll-cGviT',
+        'center': (date.today() + timedelta(days=1)).strftime('%Y-%m-%d'),
+        'count': 1
+    }
     resp = requests.post(
-        "https://api.skaping.com//media/search",data=request_data).json()
+        "https://api.skaping.com//media/search", data=request_data).json()
     return resp
+
 
 def send_message(bot: telegram.Bot, msg: str, chat_id: int):
     bot.send_message(text=msg, chat_id=chat_id)
+
 
 def send_image(bot: telegram.Bot, photo: str, chat_id: int, caption: str):
     bot.send_photo(chat_id=chat_id, photo=photo, caption=caption)
