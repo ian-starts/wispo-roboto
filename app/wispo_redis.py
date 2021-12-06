d3e7e77ef8bd17dbffab06668a30093b8afc694f
Yoni Ian Kok <hello@yik.dev>
2021-12-06 19:52:36 +0100

    different redis constructor

--- app/wispo_redis.py
+++ app/wispo_redis.py
@@ -5,8 +5,8 @@ import redis
 
 url = urlparse("redis://:p474a84c390827126039221d0a48c070610bcaefddc6ca3b3bdb4d6b1ab3a0eb0@ec2-54-74-153-24.eu-west-1"
                ".compute.amazonaws.com:17940")
-r = redis.from_url(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True,
-                   ssl_cert_reqs=None)
+r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True,
+                ssl_cert_reqs=None)
 
 
 def set_location(user_id, lat, long):
