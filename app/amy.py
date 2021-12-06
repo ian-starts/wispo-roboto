5b7ec2b88c8f947db6cf6a85071d017a5758e339
Yoni Ian Kok <hello@yik.dev>
2021-12-06 20:04:22 +0100

    add error

--- app/amy.py
+++ app/amy.py
@@ -34,14 +34,17 @@ def get_manly():
 
 
 def get_location(user_id):
-    api_key = 'fY6o1AeYGyi55iMzO9q_A1EPhcuawutvHKJSQ6Mx4dY'
-    location = get_redis_location(user_id)
-    car_route_url = "https://router.hereapi.com/v8/routes"
-    car_route_params = {'apikey': api_key, 'transportMode': 'car', 'origin': f"{location['lat']},{location['lon']}",
-                        'destination': '45.01331, 6.12471', 'return': 'summary'}
-    route_req = requests.get(url=car_route_url, params=car_route_params)
-    route_data = route_req.json()
-    travel_time = route_data["routes"][0]["sections"][0]["summary"]["duration"] / 3600
-    travel_distance = route_data["routes"][0]["sections"][0]["summary"]["length"] / 1000
-    return "You are " + str(round(travel_time, 2)) + " hours and " + str(
-        round(travel_distance)) + " kilometers away by car from your WISPO destination: Les Deux Alpes ⛷️🏂"
+    try:
+        api_key = 'fY6o1AeYGyi55iMzO9q_A1EPhcuawutvHKJSQ6Mx4dY'
+        location = get_redis_location(user_id)
+        car_route_url = "https://router.hereapi.com/v8/routes"
+        car_route_params = {'apikey': api_key, 'transportMode': 'car', 'origin': f"{location['lat']},{location['lon']}",
+                            'destination': '45.01331, 6.12471', 'return': 'summary'}
+        route_req = requests.get(url=car_route_url, params=car_route_params)
+        route_data = route_req.json()
+        travel_time = route_data["routes"][0]["sections"][0]["summary"]["duration"] / 3600
+        travel_distance = route_data["routes"][0]["sections"][0]["summary"]["length"] / 1000
+        return "You are " + str(round(travel_time, 2)) + " hours and " + str(
+            round(travel_distance)) + " kilometers away by car from your WISPO destination: Les Deux Alpes ⛷️🏂"
+    except:
+        return "Something went fucky, did you share your location with me?"
